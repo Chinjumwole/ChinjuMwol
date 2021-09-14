0d517da0ce4373715d2eb2d626598b47ba3b48b4
CHINJUMWOL <90092049+Ajnas-dz@users.noreply.github.com>
2021-09-14 13:36:37 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/bNYbdXa.jpeg";
+  r_text[0] = "https://i.imgur.com/07KGGa8.jpeg";
     
     
     var i = Math.floor(1*Math.random())
